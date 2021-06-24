26c4b96ac985f3825a78177ccde81e6dc82eb4a7
SIVA SHANKARAN V <71275854+sivashankaran04@users.noreply.github.com>
2021-06-24 19:57:15 +0530

    Add files via upload

---
+++ day-12 task.py
@@ -0,0 +1,29 @@
+
+# MODULE : list1 = ['Deepak', 'akkilan', 'Shiva', 'Balaji']
+# def d():
+#    print('New list', list1)
+import list
+list.list1[3] = 'THE BOYS'
+print(list.d())
+
+# 2
+
+import pandas
+print(pandas)
+
+# 3
+import numpy as num
+a = num.random.randint(low=1, high=100)
+b = num.random.randint(low=1, high=100)
+c = num.random.randint(low=1, high=100, size=10)
+print('Random number is ', a)
+print('Random number is ', b)
+print('Random number is ', c)
+
+# 4
+import sys
+print(sys.path)
+
+# 5
+
+print('\nnumpy package is uninstalled and again installed using pip')
