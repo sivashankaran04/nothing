c6dbab901b5e8541aec31dd00fde1de4e20aa75e
SIVA SHANKARAN V <71275854+sivashankaran04@users.noreply.github.com>
2021-06-24 20:04:51 +0530

    Update Day-12 task.py

--- Day-12 task.py
+++ Day-12 task.py
@@ -1,6 +1,6 @@
 f = open('30 days 30 hour operations', 'a')
 f.write('I have completed 10 days successfully ')
-f.write('by Deepak.G')
+f.write('by SIVA SHANKARAN V')
 f = open('30 days 30 hour operations', 'r')
 print(f.read())
 f.close()
