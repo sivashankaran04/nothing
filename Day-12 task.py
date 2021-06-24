bd93f902904e78da5a58c57d2f891262f583f64d
SIVA SHANKARAN V <71275854+sivashankaran04@users.noreply.github.com>
2021-06-24 20:02:35 +0530

    Add files via upload

---
+++ Day-12 task.py
@@ -0,0 +1,6 @@
+f = open('30 days 30 hour operations', 'a')
+f.write('I have completed 10 days successfully ')
+f.write('by Deepak.G')
+f = open('30 days 30 hour operations', 'r')
+print(f.read())
+f.close()
