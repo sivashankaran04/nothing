9d7366b71409d1643336455e3d00dc72545bc2f0
SIVA SHANKARAN V <71275854+sivashankaran04@users.noreply.github.com>
2021-06-24 20:03:17 +0530

    Delete Day-12 task.py

--- Day-12 task.py
+++
@@ -1,6 +0,0 @@
-f = open('30 days 30 hour operations', 'a')
-f.write('I have completed 10 days successfully ')
-f.write('by Deepak.G')
-f = open('30 days 30 hour operations', 'r')
-print(f.read())
-f.close()
